a06eae9a0591887c3230378f00484cdac9efc001
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-07-14 10:12:20 +0900

    connecting postgresql db

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const imageDetect = require ('./controllers/imageDetect')
 const db= knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'test',
-      database : 'smart-brain'
+    connectionString : 'process.env.DATABASE_URL',
+    ssl:true
     }
   });
 //Setup express and cors
