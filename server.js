38d8bf8cab6657514234c70e91737d583d5d5979
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-07-14 23:35:41 +0900

    edited host for db

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const imageDetect = require ('./controllers/imageDetect')
 const db= knex({
     client: 'pg',
     connection: {
-    connectionString : 'process.env.DATABASE_URL',
+    connectionString : process.env.DATABASE_URL,
     ssl:true
     }
   });
