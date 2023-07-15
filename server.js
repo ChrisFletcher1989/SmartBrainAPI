0400e772851d96293156c7e94211607b4055d310
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-07-15 16:07:02 +0900

    added auth info

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ const imageDetect = require ('./controllers/imageDetect')
 
 
 //Connect to database with knex
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 const db= knex({
     client: 'pg',
     connection: {
     connectionString: process.env.DATABASE_URL,
     ssl:true
     }
-  });
+  })
 //Setup express and cors
 const app = express();
 app.use(express.json());
@@ -35,3 +36,5 @@ app.post('/imagedetect', (req, res,) => {imageDetect.handleApiCallRecognize(req,
 app.listen(process.env.PORT || 3000, () => {
     console.log(`running OK on port ${process.env.PORT}`)
 })
+// CREATE TABLE login (id serial PRIMARY KEY, hash varchar(100) NOT NULL, email text UNIQUE NOT NULL);
+// CREATE TABLE users (id serial PRIMARY KEY, name varchar(100), email text UNIQUE NOT NULL, entries BIGINT DEFAULT 0, joined TIMESTAMP NOT NULL);
